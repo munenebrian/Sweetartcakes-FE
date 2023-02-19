9771df46f561c4bb527fef360a01ad44ce5f73b4
Job Karani <jobkarani084j@gmail.com>
2023-02-19 17:54:50 +0300

    feat: navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,29 +1,20 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import * as $ from 'jquery';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent {
 
-  constructor() { }
+  constructor() {};
 
-  ngOnInit(): void {
-    $(document).ready(function () {
-      $(window).scroll(function () {
-                var height = $(".navigation").height();
-                var scrollTop = $(window).scrollTop();
-                console.log("Scroll");
-      
-                if (scrollTop! >= height! - 70) {
-                  $(".navigation").css("background-color", "#251104");
-                } else {
-                  $(".navigation").css("background-color", "transparent");
-                }
-              });
-      });
+  menuVar: boolean = false;
+  menu_icon_var: boolean = false;
+
+  openMenu(){
+    this.menuVar =! this.menuVar;
+    this.menu_icon_var =! this.menu_icon_var;
   }
 
 }
