f574c8021fe194f4c52d5ef3587068f3d8e64056
Job Karani <jobkarani084j@gmail.com>
2023-02-24 16:30:48 +0300

    feat: blogs and blogdetails functionality

---
+++ src/app/interfaces/blogs.ts
@@ -0,0 +1,9 @@
+export interface Blogs {
+    id:number;
+    image:string;
+    heading:string;
+    created_at:Date;
+    text:string;
+    tex2:string;
+    text3:string;
+}
---
+++ src/app/interfaces/category-blogs.ts
@@ -0,0 +1,14 @@
+export interface CategoryBlogs {
+    id:number;
+    image:string;
+    heading:string;
+    created_at:Date;
+    text:string;
+    tex2:string;
+    text3:string;
+    category_name:string;
+    category_slug:string;
+    is_available:boolean;
+    get_url:string;
+    category_id:number;
+}
